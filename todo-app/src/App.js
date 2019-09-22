fa26fdaf4a36c480244b7824b720baee7db11790
MicahJank <MicahJank@users.noreply.github.com>
2019-09-22 15:01:23 -0500

    styled the components

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -3,13 +3,17 @@ import React from 'react';
 import Form from './components/Form.js';
 import TodoList from './components/TodoList.js';
 
+import { Container } from 'semantic-ui-react';
+
 import { TodoProvider } from './context/TodoContext.js';
 
 function App() {
   return (
     <TodoProvider>
-      <Form />
-      <TodoList />
+      <Container textAlign='center'>
+        <Form />
+        <TodoList />
+      </Container>
     </TodoProvider>
   );
 }
--- todo-app/src/components/Form.js
+++ todo-app/src/components/Form.js
@@ -1,6 +1,34 @@
 import React, { useContext, useState } from 'react';
 import { TodoContext } from '../context/TodoContext.js';
 
+import { Button, Form as TodoForm } from 'semantic-ui-react';
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+    .ui.form .todo-form {
+        border-top: none;
+        border-left: none;
+        border-right: none;
+        width: 50%;
+        border-radius: 0;
+    }
+
+    .ui.form {
+        margin-top: 20px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
+
+    .ui.button.add {
+        margin-left: 10px;
+    }
+
+    .ui.button.clear-completed {
+        margin: 20px;
+    }
+`;
 
 const Form = () => {
 
@@ -11,21 +39,27 @@ const Form = () => {
         setTodoText(e.target.value);
     }
 
+    const handleSubmit = () => {
+        dispatch({ type: 'ADD', payload: todoText});
+        setTodoText('');
+    }
+
     return (
-        <div>
-            <input
-                type='text'
-                name={todoText}
-                value={todoText}
-                onChange={handleChanges}
-             />
-             <button onClick={() => {
-                 dispatch({ type: 'ADD', payload: todoText})
-             }}>Add</button>
-             <button onClick={() => {
-                 dispatch({type: 'CLEAR_COMPLETED'})
-             }}>Clear Completed</button>
-        </div>
+        <Container>
+            <TodoForm>
+                <input className='todo-form'
+                    type='text'
+                    name={todoText}
+                    value={todoText}
+                    onChange={handleChanges}
+                    placeholder='Enter a todo...'
+                />
+                <Button primary className='add' type='submit' onClick={handleSubmit}>Add</Button>
+            </TodoForm>
+            <Button color='yellow' className='clear-completed' onClick={() => {
+                    dispatch({type: 'CLEAR_COMPLETED'})
+                }}>Clear Completed</Button>
+        </Container>
     );
 }
 
